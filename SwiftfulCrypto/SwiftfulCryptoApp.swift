41734e10d24e5d1986d38c16664af09d109ecc95
Evans Nguyen <thanhtrungnguyen1208@gmail.com>
2023-07-21 16:01:23 +0700

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Trung Nguyen on 21/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
