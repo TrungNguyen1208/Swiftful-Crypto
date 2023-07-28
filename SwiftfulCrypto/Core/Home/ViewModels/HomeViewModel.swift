ae1bf0b53e64c810c93e9e4e578b64f92c0b4468
Evans Nguyen <thanhtrungnguyen1208@gmail.com>
2023-07-28 17:32:03 +0700

    Download coins using a View Model and Data Service class

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -7,16 +7,25 @@
 
 import Foundation
 import SwiftUI
+import Combine
 
 final class HomeViewModel: ObservableObject {
   
   @Published var allCoins: [CoinModel] = []
   @Published var portfolioCoins: [CoinModel] = []
   
+  private let dataService = CoinDataService()
+  private var cancellables = Set<AnyCancellable>()
+  
+  
   init() {
-    DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
-      self?.allCoins.append(DeveloperPreview.instance.coin)
-      self?.portfolioCoins.append(DeveloperPreview.instance.coin)
+    addSubscribers()
+  }
+  
+  func addSubscribers() {
+    dataService.$allCoins.sink { [weak self] coins in
+      self?.allCoins = coins
     }
+    .store(in: &cancellables)
   }
 }
---
+++ SwiftfulCrypto/Services/CoinDataService.swift
@@ -0,0 +1,47 @@
+//
+//  CoinDataService.swift
+//  SwiftfulCrypto
+//
+//  Created by Trung Nguyen on 28/07/2023.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+  @Published var allCoins: [CoinModel] = []
+  
+  var coinSubcription: AnyCancellable?
+  
+  init() {
+    getCoins()
+  }
+  
+  private func getCoins() {
+    guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en") else {
+      return
+    }
+    
+    coinSubcription = URLSession.shared.dataTaskPublisher(for: url)
+      .subscribe(on: DispatchQueue.global(qos: .default))
+      .tryMap { (output) -> Data in
+        guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
+          throw URLError(.badServerResponse)
+        }
+        return output.data
+      }
+      .receive(on: DispatchQueue.main)
+      .decode(type: [CoinModel].self, decoder: JSONDecoder())
+      .sink { completion in
+        switch completion {
+        case .finished:
+          break
+        case .failure(let error):
+          print(error.localizedDescription)
+        }
+      } receiveValue: { [weak self] coins in
+        self?.allCoins = coins
+        self?.coinSubcription?.cancel()
+      }
+  }
+}
