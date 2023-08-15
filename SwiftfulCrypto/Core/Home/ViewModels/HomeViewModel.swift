02072670023b5c9edc24aecbeb542a3a01e9789e
Trung <trungnt@hybrid-technologies.vn>
2023-08-15 15:53:29 +0700

    Filtering data based on search bar text using Combine

--- SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftfulCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -13,7 +13,7 @@ final class HomeViewModel: ObservableObject {
   
   @Published var allCoins: [CoinModel] = []
   @Published var portfolioCoins: [CoinModel] = []
-    
+  
   @Published var searchText: String = ""
   
   private let dataService = CoinDataService()
@@ -25,9 +25,29 @@ final class HomeViewModel: ObservableObject {
   }
   
   func addSubscribers() {
-    dataService.$allCoins.sink { [weak self] coins in
-      self?.allCoins = coins
-    }
-    .store(in: &cancellables)
+    $searchText
+      .combineLatest(dataService.$allCoins)
+      .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+      .map(filterCoins)
+      .sink { [weak self] coins in
+        self?.allCoins = coins
+      }
+      .store(in: &cancellables)
   }
+}
+
+private extension HomeViewModel {
+  
+  func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+    guard text.isNotEmpty else {
+      return coins
+    }
+    let lowercasedText = text.lowercased()
+    return coins.filter {
+      $0.name.lowercased().contains(lowercasedText) ||
+      $0.symbol.lowercased().contains(lowercasedText) ||
+      $0.id.lowercased().contains(lowercasedText)
+    }
+  }
+  
 }
